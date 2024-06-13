3a0b78d9c0abea6588bb8f28156e8f5d30262d29
Alif Triadi <aliftriadi.gaming@gmail.com>
2024-06-13 20:55:08 +0700

    feat(auth): removing confirm_password from validation when registering

--- src/auth/AuthSchema.ts
+++ src/auth/AuthSchema.ts
@@ -1,4 +1,4 @@
-import { object, string, pipe, email, minLength, InferInput, forward, check } from "valibot";
+import { object, string, pipe, email, minLength, InferInput } from "valibot";
 
 export const loginSchema = object({
     email: pipe(string("your email must be a string"), email("your email must be a valid email")),
@@ -7,18 +7,11 @@ export const loginSchema = object({
 
 export type LoginSchema = InferInput<typeof loginSchema>;
 
-export const registerSchema = pipe(object({
+export const registerSchema = object({
     name: string("your name must be a string"),
     email: pipe(string("your email must be a string"), email("your email must be a valid email")),
-    password: pipe(string("your password must be a string"), minLength(8)),
-    confirm_password: string()
-}), forward(
-    check(
-        (input) => input.password === input.confirm_password,
-        'The confirm password doesn\'t match'
-    ),
-    ['confirm_password']
-))
+    password: pipe(string("your password must be a string"), minLength(8))
+})
 
 export type RegisterSchema = InferInput<typeof registerSchema>;
 
